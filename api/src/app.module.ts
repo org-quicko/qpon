a8dcc1e3753a26bc785d27bcf0ccb34064a8a424
Dev Parikh <dev.parikh@quicko.com>
2025-04-07 16:48:07 +0530

    updated dependencies and dockerized the application

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -29,7 +29,7 @@ import { CouponCodeSubscriber } from './subscribers/coupon-code.subscriber';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT ?? '5432'),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
