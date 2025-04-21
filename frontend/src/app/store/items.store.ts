94b2df30d07033426fb4619bb1d5782e236ad05e
Dev Parikh <dev.parikh@quicko.com>
2025-04-21 18:06:22 +0530

    fixed: pagination bug in item store

--- frontend/src/app/store/items.store.ts
+++ frontend/src/app/store/items.store.ts
@@ -6,16 +6,16 @@ import { catchError, concatMap, EMPTY, of, pipe, shareReplay, tap } from 'rxjs';
 import { tapResponse } from '@ngrx/operators';
 import { ItemDto } from '../../dtos/item.dto';
 import { plainToClass, plainToInstance } from 'class-transformer';
-import { withDevtools } from "@angular-architects/ngrx-toolkit";
+import { withDevtools } from '@angular-architects/ngrx-toolkit';
 import { PaginatedList } from '../../dtos/paginated-list.dto';
 import { ItemFilter } from '../types/item-filter.interface';
 import { HttpErrorResponse } from '@angular/common/http';
 
 type ItemsState = {
   items: ItemDto[];
-  skip?: number | null,
-  take?: number | null,
-  count?: number | null,
+  skip?: number | null;
+  take?: number | null;
+  count?: number | null;
   isLoading: boolean;
   filter: { query: string } | null;
   loadedPages: Set<number>;
@@ -38,56 +38,75 @@ export const ItemsStore = signalStore(
   withDevtools('items'),
   withState(initialState),
   withMethods((store, itemsService = inject(ItemsService)) => ({
-    fetchItems: rxMethod<{ organizationId: string, skip?: number, take?: number, filter?: ItemFilter }>(
+    fetchItems: rxMethod<{
+      organizationId: string;
+      skip?: number;
+      take?: number;
+      filter?: ItemFilter;
+    }>(
       pipe(
         tap(() => patchState(store, { isLoading: true })),
         concatMap(({ organizationId, skip, take, filter }) => {
+          const page = Math.floor((skip ?? 0) / (take ?? 10));
 
-          const page = Math.floor((skip ?? 0) / (take ?? 5));
-
-          if(store.loadedPages().has(page) && !filter) {
-            patchState(store, { isLoading: false })
+          if (store.loadedPages().has(page) && !filter) {
+            patchState(store, { isLoading: false });
             return of(store.items());
           }
 
-          return itemsService.fetchItems(organizationId, skip, take, filter).pipe(
-            tapResponse({
-              next: (response) => {
-                const itemList = plainToClass(PaginatedList<ItemDto>, response.data);
-                const items = itemList.getItems()?.map((item) => plainToInstance(ItemDto, item))
+          return itemsService
+            .fetchItems(organizationId, skip, take, filter)
+            .pipe(
+              tapResponse({
+                next: (response) => {
+                  const itemList = plainToClass(
+                    PaginatedList<ItemDto>,
+                    response.data
+                  );
+                  const items = itemList
+                    .getItems()
+                    ?.map((item) => plainToInstance(ItemDto, item));
 
-                const updatedPages = store.loadedPages().add(page);
-                const currentItems = store.items() ?? [];
-                let updatedItems: ItemDto[] = [];
+                  const updatedPages = store.loadedPages().add(page);
+                  const currentItems = store.items() ?? [];
+                  let updatedItems: ItemDto[] = [];
 
-                if (filter) {
-                  updatedItems = items!;
-                } else {
-                  updatedItems = [...currentItems, ...items!];
-                }
+                  if (filter) {
+                    updatedItems = items!;
+                  } else {
+                    updatedItems = [...currentItems, ...items!];
+                  }
 
-                patchState(store, {
-                  items: updatedItems,
-                  skip: itemList?.getSkip(),
-                  count: itemList?.getCount(),
-                  take: itemList?.getTake(),
-                  loadedPages: updatedPages,
-                  isLoading: false,
-                });
-              },
-              error: (error: HttpErrorResponse) => {
-                if(error.status === 404) {
-                  patchState(store, { items: [], isLoading: false });
-                } else {
-                  patchState(store, { isLoading: false, error: error.message });
-                }
-              },
-            }),
-            catchError((error) => {
-              patchState(store, { isLoading: false, error: error.message });
-              return EMPTY;
-            })
-          );
+                  patchState(store, {
+                    items: updatedItems,
+                    skip: itemList?.getSkip(),
+                    count: itemList?.getCount(),
+                    take: itemList?.getTake(),
+                    loadedPages: updatedPages,
+                    isLoading: false,
+                  });
+                },
+                error: (error: HttpErrorResponse) => {
+                  if (error.status === 404) {
+                    patchState(store, {
+                      items: [],
+                      isLoading: false,
+                      loadedPages: new Set(),
+                      count: 0,
+                    });
+                  } else {
+                    patchState(store, {
+                      isLoading: false,
+                      error: error.message,
+                    });
+                  }
+                },
+              }),
+              catchError((error) => {
+                patchState(store, { isLoading: false, error: error.message });
+                return EMPTY;
+              })
+            );
         })
       )
     ),
@@ -100,7 +119,9 @@ export const ItemsStore = signalStore(
               next: (response) => {
                 if (response.code === 200) {
                   patchState(store, {
-                    items: store.items().filter((item) => item.itemId !== itemId),
+                    items: store
+                      .items()
+                      .filter((item) => item.itemId !== itemId),
                   });
                 }
               },
