e82df8c7a29932c77f69699986203488c7b89909
Dev Parikh <dev.parikh@quicko.com>
2025-05-13 11:39:57 +0530

    removed: app controller for serving static files

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,14 +1,7 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Response } from 'express';
-import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  root(@Res() response: Response): void {
-    response.sendFile(join(__dirname, '..', '..', 'public', 'index.html'));
-  }
 }
