edf709bc11603afbba232d5676e878fb64322808
Dev79844 <parikhdev13@gmail.com>
2025-09-29 18:42:29 +0530

    updated: docusaurus config

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -1,22 +1,8 @@
 import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 import generatedSidebar from './docs/sidebar';
 
-const makeCollapsible = (sidebar: any[]): any[] => {
-  return sidebar.map(item => {
-    if (item.type === 'category') {
-      return {
-        ...item,
-        collapsible: true,
-        collapsed: true,
-        items: item.items ? makeCollapsible(item.items) : item.items
-      };
-    }
-    return item;
-  });
-};
-
 const sidebars: SidebarsConfig = {
-  apiSidebar: makeCollapsible(generatedSidebar),
+  apiSidebar: generatedSidebar,
 };
 
 export default sidebars;
